7e6ed3e7323db53ad9e18008c5733b497c6a1232
syn'thix <84481726+systemsculptor@users.noreply.github.com>
2024-02-02 14:55:34 -0700

    qqq

--- echowalker/firmware/ultrasonic/UltrasonicCode/UltrasonicCode.ino
+++ echowalker/firmware/ultrasonic/UltrasonicCode/UltrasonicCode.ino
@@ -1,24 +1,36 @@
 
-float duration, distance;
 
 
 
-
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+void setup(){
+  Serial.println("begining");
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int trigPin = D4;
+  int echoPin = D5;
+
+  // put your setup code here, to run once:
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
+
+  Serial.print(trigPin);
+
   digitalWrite(trigPin, LOW);
 
+
+  Serial.print(trigPin);
+
+
+  float duration;
+  float distance;
   duration = pulseIn(echoPin, HIGH);
   distance = (duration * .0343) / 2;
 
