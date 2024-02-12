be8c6077f4e5e644e5c2f32cd70eaa389dfdda75
syn'thix <84481726+systemsculptor@users.noreply.github.com>
2024-02-12 14:52:07 -0700

    new

--- echowalker/firmware/main/main.ino
+++ echowalker/firmware/main/main.ino
@@ -11,7 +11,16 @@ void setUp()
 void loop()
 {
 
+serial.print("Starting ultrasonic sensors");
 
+
+
+
+serial.print("starting bluetooth");
+
+serial.print("starting sound");
+
+serial.print("Sucessfully compiled all files")
 
 
 
