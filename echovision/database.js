7be6305d093c84636fe484452a23f5edbc430191
syn'thix <84481726+systemsculptor@users.noreply.github.com>
2024-04-18 14:57:44 -0600

    ASDASD

--- echovision/database.js
+++ echovision/database.js
@@ -1,10 +1,29 @@
 const mongoose = require('mongoose');
 
-main().catch(err => console.log(err));
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
 
-async function main() 
-{
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
 
+const Course = mongoose.model('Course', courseSchema);
 
-    
+async function createCourse() {
+  const course = new Course({
+    name: 'Node.js Course',
+    author: 'John Doe',
+    tags: ['node', 'backend'],
+    isPublished: true
+  });
+
+  const result = await course.save();
+  console.log(result);
 }
+
+createCourse();
